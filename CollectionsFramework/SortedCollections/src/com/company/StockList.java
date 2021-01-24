494a01e1b8be9179cfcbbb09bd706325d0f1cfd2
JustinSHong <jhong2009@gmail.com>
2021-01-24 13:17:45 -0800

    set up StockItem, StockList, Basket models and add items to stock list

---
+++ CollectionsFramework/SortedCollections/src/com/company/StockList.java
@@ -0,0 +1,71 @@
+package com.company;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class StockList {
+    private final Map<String, StockItem> list;
+
+    public StockList() {
+        this.list = new LinkedHashMap<>();
+    }
+
+    public int addStock(StockItem item) {
+        if (item != null) {
+            // check if we already have quantities of this item
+            // get item if it exists or return the default
+            StockItem inStock = list.getOrDefault(item.getName(), item);
+            // if item has stock, then adjust the quantity
+            if (inStock != item) {
+                item.adjustStock(inStock.quantityInStock());
+            }
+
+            list.put(item.getName(), item);
+            return item.quantityInStock();
+        }
+
+        return 0; // no stock exists for the given item
+    }
+
+    public int sellStock(String itemName, int quantity) {
+        StockItem inStock = list.getOrDefault(itemName, null);
+
+        if (inStock != null && inStock.quantityInStock() >= quantity && quantity > 0) {
+            inStock.adjustStock(-quantity);
+            return quantity;
+        }
+        return 0; // stock not adjusted
+    }
+
+    public StockItem get(String itemName) {
+        return list.get(itemName);
+    }
+
+    // Collections framework provides wrappers around List, Map, Set
+    // to create unmodifiable collections
+    // another way to obtain immutability - this is "read-only"
+    // this is faster since we don't have to make a copy
+    // clients can not modify the returned collections
+    public Map<String, StockItem> Items() {
+        return Collections.unmodifiableMap(list);
+    }
+
+    @Override
+    public String toString() {
+        String s = "\n===Stock List=== \n";
+        double totalCost = 0.0;
+
+        for (Map.Entry<String, StockItem> item : list.entrySet()) {
+            StockItem stockItem = item.getValue();
+
+            double itemValue = stockItem.getPrice() * stockItem.quantityInStock();
+
+            s = s + stockItem + " .There are " + stockItem.quantityInStock() + " in stock. Value of items: ";
+            s = s + String.format("%.2f", itemValue) + "\n";
+
+            totalCost += itemValue;
+        }
+        return s + "Total stock value " + totalCost;
+    }
+}
